f7f4eda30d44b6e7cae3b07b0a992f9a83a1d39f
swd-naka <YusukeC.Nakamura@jp.sony.com>
2015-08-18 20:54:27 +0900

    add json get method using Weatherget free API

--- app/src/main/java/com/example/kentaro/fashioncoordinator/FashionHistory.java
+++ app/src/main/java/com/example/kentaro/fashioncoordinator/FashionHistory.java
@@ -3,8 +3,11 @@ package com.example.kentaro.fashioncoordinator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.kentaro.fashioncoordinator.WeatherManager.WeatherManager;
 
 /**
  * Created by 0000100038 on 2015/07/22.
@@ -15,6 +18,17 @@ public class FashionHistory extends Activity {
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.fashion_history);
+
+        Log.d("debug_test ", "push!!");
+        /*
+        WeatherManager WM = new WeatherManager();
+        String json_result;
+        json_result = WM.weather_method();*/
+
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp";
+        WeatherManager task = new WeatherManager(this);
+        task.owner = this;
+        task.execute(url);
 
         // ƒ{ƒ^ƒ“’è‹`
         Button buttonScan = (Button)findViewById(R.id.button_history_back);
--- app/src/main/java/com/example/kentaro/fashioncoordinator/WeatherManager/WeatherManager.java
+++ app/src/main/java/com/example/kentaro/fashioncoordinator/WeatherManager/WeatherManager.java
@@ -1,8 +1,65 @@
 package com.example.kentaro.fashioncoordinator.WeatherManager;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.io.IOException;
+import java.text.ParseException;
+
 /**
  * Created by 0000131258 on 2015/08/11.
  */
-public class WeatherManager {
+public class WeatherManager extends AsyncTask<String, Void, String> {
+    public Activity owner;
+    private String ReceiveStr;
+
+    public WeatherManager(Activity activity) {
+        owner = activity;
+    }
+
+    @Override
+    protected String doInBackground(String... url) {
+        try {
+            HttpGet httpGet = new HttpGet(url[0]);
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            httpGet.setHeader("Connection", "Keep-Alive");
+
+            HttpResponse response = httpClient.execute(httpGet);
+            int status = response.getStatusLine().getStatusCode();
+            if (status != HttpStatus.SC_OK) {
+                throw new Exception("");
+            } else {
+                ReceiveStr = EntityUtils.toString(response.getEntity(), "UTF-8");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+       // TextView textView2 = (TextView) owner.findViewById(R.id.textView2);
+        //textView2.setText(ReceiveStr);
+        Log.d("debug_test",ReceiveStr);
+    }
+
+
 }
-git 
