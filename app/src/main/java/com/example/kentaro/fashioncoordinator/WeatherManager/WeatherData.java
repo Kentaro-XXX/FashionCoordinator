2a3ae5060de997be1500e3fbc299627bc54b8791
swd-naka <YusukeC.Nakamura@jp.sony.com>
2015-08-21 20:45:38 +0900

    git add WeatherDate

---
+++ app/src/main/java/com/example/kentaro/fashioncoordinator/WeatherManager/WeatherData.java
@@ -0,0 +1,10 @@
+package com.example.kentaro.fashioncoordinator.WeatherManager;
+
+/**
+ * Created by 0000131258 on 2015/08/21.
+ */
+public class WeatherData {
+    public String temp;
+    public String weather;
+    public String humidity;
+}
